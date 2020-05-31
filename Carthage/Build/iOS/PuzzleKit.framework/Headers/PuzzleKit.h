c2e4cbd552c5561b15fa087d047b048d941dfa2b
Nikhil Sharma <nikksindia@gmail.com>
2020-05-31 13:46:15 +0530

    Added support for Carthage

---
+++ Carthage/Build/iOS/PuzzleKit.framework/Headers/PuzzleKit.h
@@ -0,0 +1,19 @@
+//
+//  PuzzleKit.h
+//  PuzzleKit
+//
+//  Created by Nikhil Sharma on 31/05/20.
+//  Copyright © 2020 Nikhil Sharma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PuzzleKit.
+FOUNDATION_EXPORT double PuzzleKitVersionNumber;
+
+//! Project version string for PuzzleKit.
+FOUNDATION_EXPORT const unsigned char PuzzleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PuzzleKit/PublicHeader.h>
+
+
